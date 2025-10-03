24ea039679630e5559409b53665faaf841d6092c
INDONESIA KID <siyagamertech@gmail.com>
2025-10-04 00:35:52 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: '𝐈𝐍𝐃𝐎𝐍𝐄𝐒𝐈𝐀 𝐊𝐈𝐃 𝐌𝐃 𝐕𝟐',
+  author: '‎Gods Zeal †',
+  botName: "𝐈𝐍𝐃𝐎𝐍𝐄𝐒𝐈𝐀 𝐊𝐈𝐃 𝐌𝐃 𝐕𝟐",
+  botOwner: 'Indonesia kid†', // Your name
+  ownerNumber: '27665321966', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+  updateZipUrl: "https://github.com/DARKNESSKING954/INDONESIA-KID-MDV2/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
